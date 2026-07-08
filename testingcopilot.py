d88e2b4fc65f052ef2f286c2125890cac4d84a02
Evan Lin <evanzkailin@gmail.com>
2026-07-08 11:00:13 -0400

    adding hangman files

--- testingcopilot.py
+++
@@ -1,9 +0,0 @@
-def main():
-	first = float(input("Enter the first number: "))
-	second = float(input("Enter the second number: "))
-	product = first * second
-	print(f"Product: {product}")
-
-
-if __name__ == "__main__":
-	main()
